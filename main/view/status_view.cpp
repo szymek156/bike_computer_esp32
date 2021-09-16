a1a02858195e10a1d6fe7dfbfa48fbced3285706
Szymon Zimnowoda <szymon.zimnowoda@gmail.com>
2021-09-16 12:01:36 +0200

    using generated gui

--- main/view/status_view.cpp
+++ main/view/status_view.cpp
@@ -10,33 +10,47 @@ StatusView::StatusView(IDisplay *display) : display_(display) {
 void StatusView::drawStatic() {
     display_->enqueueStaticDraw(
         [](Paint &paint) {
-            paint.DrawHorizontalLine(4, 13, paint.GetHeight() - 4, COLORED);
+            paint.DrawVerticalLine(98, 3, 6, COLORED);
+            paint.DrawVerticalLine(196, 3, 6, COLORED);
         },
         // Rectangle needs to cover whole widget area
         {0, 0, display_->getWidth(), 13});
 }
 
 void StatusView::drawGNSSData(const GNSSData &data) {
+    // GPS 3D
     display_->enqueueDraw(
         [&data](Paint &paint) {
             const int msg_size = 128;
             char message[msg_size];
 
             snprintf(message, msg_size, "GPS %1d", data.fix_status);
-            paint.DrawStringAt(180, 4, message, &Font8, COLORED);
+            paint.DrawStringAt(132, 2, message, &Font8, COLORED);
         },
-        {180, 4, 210, 10});
+        {99, 1, 195, 11});
 }
 
 void StatusView::drawDateTime(const tm &time_info) {
+    // 21:37
     display_->enqueueDraw(
-        [&time_info](Paint &paint) {
+        [&](Paint &paint) {
             const int msg_size = 128;
             char message[msg_size];
 
-            strftime(message, msg_size, "%d/%m/%y %T", &(time_info));
-            paint.DrawStringAt(4, 4, message, &Font8, COLORED);
+            strftime(message, msg_size, "%T", &(time_info));
+            paint.DrawStringAt(36, 2, message, &Font8, COLORED);
         },
-        {4, 4, 100, 10});
+        {1, 1, 97, 11});
+
+    // 02/09/21
+    display_->enqueueDraw(
+        [&](Paint &paint) {
+            const int msg_size = 128;
+            char message[msg_size];
+
+            strftime(message, msg_size, "%d/%m/%y", &(time_info));
+            paint.DrawStringAt(225, 2, message, &Font8, COLORED);
+        },
+        {197, 1, 293, 11});
 }
 }  // namespace bk
--- main/view/welcome_view.cpp
+++ main/view/welcome_view.cpp
@@ -10,73 +10,99 @@ WelcomeView::WelcomeView(IDisplay *display) : display_(display) {
 void WelcomeView::drawStatic() {
     display_->enqueueStaticDraw(
         [](Paint &paint) {
-            // TODO: Those arcane computations shall be done by tool
-            const int MARGIN = 8;
-            paint.DrawHorizontalLine(
-                MARGIN, paint.GetWidth() / 2 + 6, paint.GetHeight() - 2 * MARGIN, COLORED);
-            paint.DrawVerticalLine(
-                paint.GetHeight() / 2, 13 + MARGIN, paint.GetWidth() - 13 - 2 * MARGIN, COLORED);
+            paint.DrawHorizontalLine(13, 12, 270, COLORED);
+            paint.DrawVerticalLine(148, 15, 52, COLORED);
+            paint.DrawHorizontalLine(13, 70, 270, COLORED);
+            paint.DrawVerticalLine(148, 73, 52, COLORED);
 
             const int msg_size = 128;
             char message[msg_size];
 
+            // Hit button below
             snprintf(message, msg_size, "Hit button below");
-            paint.DrawStringAt(160, 77, message, &Font12, COLORED);
+            paint.DrawStringAt(166, 73, message, &Font12, COLORED);
 
+            // to calculate your
             snprintf(message, msg_size, "to calculate your");
-            paint.DrawStringAt(160, 92, message, &Font12, COLORED);
+            paint.DrawStringAt(162, 92, message, &Font12, COLORED);
 
+            // BMI
             snprintf(message, msg_size, "BMI");
-            paint.DrawStringAt(200, 106, message, &Font16, COLORED);
-            
-            
+            paint.DrawStringAt(205, 109, message, &Font16, COLORED);
         },
         // Rectangle needs to cover whole widget area
         {0, 14, display_->getHeight(), display_->getWidth()});
 }
 
 void WelcomeView::drawWeatherData(const WeatherData &data) {
+    // 23.19[*C]
     display_->enqueueDraw(
         [&data](Paint &paint) {
             const int msg_size = 128;
             char message[msg_size];
 
             snprintf(message, msg_size, "%5.2f[*C]", data.temp_c);
-            paint.DrawStringAt(18, 77, message, &Font20, COLORED);
+            paint.DrawStringAt(11, 74, message, &Font20, COLORED);
+        },
+        {1, 71, 147, 98});
+
+    // 133.94[m]
+    display_->enqueueDraw(
+        [&data](Paint &paint) {
+            const int msg_size = 128;
+            char message[msg_size];
 
             snprintf(message, msg_size, "%5.2f[m]", data.altitude_m);
-            paint.DrawStringAt(18, 105, message, &Font20, COLORED);
+            paint.DrawStringAt(11, 103, message, &Font20, COLORED);
         },
-        {0, 78, 130, 256});
+        {1, 100, 147, 127});
 }
 
 void WelcomeView::drawDateTime(const tm &time_info) {
+    // 02/09/21
     display_->enqueueDraw(
-        [&time_info](Paint &paint) {
+        [&](Paint &paint) {
             const int msg_size = 128;
             char message[msg_size];
 
             strftime(message, msg_size, "%d/%m/%y", &(time_info));
-            paint.DrawStringAt(18, 20, message, &Font20, COLORED);
+            paint.DrawStringAt(6, 14, message, &Font24, COLORED);
+        },
+        {1, 13, 147, 40});
+
+    // 19:34:20
+    display_->enqueueDraw(
+        [&](Paint &paint) {
+            const int msg_size = 128;
+            char message[msg_size];
 
             strftime(message, msg_size, "%T", &(time_info));
-            paint.DrawStringAt(18, 47, message, &Font20, COLORED);
+            paint.DrawStringAt(6, 43, message, &Font24, COLORED);
         },
-        {18, 20, 130, 67});
+        {1, 42, 147, 69});
 }
 
 void WelcomeView::drawGNSSData(const GNSSData &data) {
+    // in view / tracked
     display_->enqueueDraw(
         [&data](Paint &paint) {
             const int msg_size = 128;
             char message[msg_size];
 
             snprintf(message, msg_size, "in view / tracked");
-            paint.DrawStringAt(160, 20, message, &Font12, COLORED);
+            paint.DrawStringAt(162, 20, message, &Font12, COLORED);
+        },
+        {149, 13, 295, 40});
+
+    // 13 / 11
+    display_->enqueueDraw(
+        [&data](Paint &paint) {
+            const int msg_size = 128;
+            char message[msg_size];
 
             snprintf(message, msg_size, "%d / %d", data.sats_in_view, data.sats_tracked);
-            paint.DrawStringAt(200, 40, message, &Font12, COLORED);
+            paint.DrawStringAt(162, 43, message, &Font24, COLORED);
         },
-        {160, 20, 256, 67});
+        {149, 42, 295, 69});
 }
 }  // namespace bk
